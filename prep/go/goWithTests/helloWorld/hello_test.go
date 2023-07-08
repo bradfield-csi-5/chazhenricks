dd19bd5ff4ffa0f425d9e04c0625ed6a40a9cf41
Chaz Henricks <chaz.henricks@getbuilt.com>
2023-07-08 06:54:05 -0500

    make default message and helper function

--- prep/go/goWithTests/helloWorld/hello_test.go
+++ prep/go/goWithTests/helloWorld/hello_test.go
@@ -1,13 +1,26 @@
-package main 
+package main
 
 import "testing"
-   
-func TestHello(t *testing.T){
-  got := Hello("Chaz")
-  want := "Hello, Chaz"
 
+func TestHello(t *testing.T) {
+	t.Run("say hello to name passed in", func(t *testing.T) {
+		got := Hello("Chaz")
+		want := "Hello, Chaz"
 
-  if got != want {
-    t.Errorf("got %q - want %q", got, want)
-  }
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when no argument passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q - want %q", got, want)
+	}
 }
