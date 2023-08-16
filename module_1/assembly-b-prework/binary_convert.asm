420dcfd445b8ee9eefa765d77975fb7ba743be38
Henricks <henricks@Henrickss-MBP.attlocal.net>
2023-08-15 20:49:54 -0500

    had to look at the solution for pengram

--- module_1/assembly-b-prework/binary_convert.asm
+++ module_1/assembly-b-prework/binary_convert.asm
@@ -1,4 +1,21 @@
 section .text
 global binary_convert
 binary_convert:
+  movzx eax, byte [rdi]
+  sub eax, '0'
+  inc rdi 
+  cmp byte [rdi], 00
+  jg add_digit
 	ret
+
+add_digit:
+  shl eax, 1
+  movzx esi, byte [rdi] 
+  sub esi, '0'
+  add eax, esi
+  inc rdi 
+  cmp byte [rdi], 00
+  jg add_digit
+	ret
+
+  
--- module_1/assembly-b-prework/pangram.asm
+++ module_1/assembly-b-prework/pangram.asm
@@ -1,4 +1,25 @@
 section .text
 global pangram
 pangram:
-	ret
+  XOR edx, edx  ;zero out register, this will be our mask 
+
+LOOP_START: ;for each new byte  
+
+  movzx ecx, byte [rdi] ;move the first char into ecx
+  cmp ecx, 0x00 ;compare incoming byte to 00
+  je END_PROGRAM ;if byte we just moved is null char, then end it 
+  or ecx, 32 ;this will force lowercase
+  sub ecx, 'a' 
+  bts edx, ecx 
+  inc rdi 
+  jmp LOOP_START
+
+
+
+END_PROGRAM:
+  xor eax, eax ; set return to 0
+  and edx, 0x03ffffff ;will be all 1s if they are the same number 
+  cmp edx, 0x03ffffff 
+  sete al ;if previous comparison is equal, then this will be set to 0, otherwise 1
+  ret
+
