e4278177a8cf32b8eb145af5edd1edd78891b7ab
Henricks <henricks@Henrickss-MBP.attlocal.net>
2023-08-16 20:20:26 -0500

    I AM A GOLDEN GOD

--- module_1/assembly-2-b-prework/fib.asm
+++ module_1/assembly-2-b-prework/fib.asm
@@ -1,4 +1,24 @@
 section .text
 global fib
-fib:
-	ret
+
+fib: 
+  cmp rdi, 1 ; rdi is the argument 1 
+  jnbe .recursive
+  mov rax, rdi ;if not greater than 1, set return register and return
+  ret
+.recursive:
+  push rdi ;push onto the stack the current state of N
+  dec rdi ;go down the n - 1 path
+  call fib
+  ;will come here when n  - 1 finally returns
+  mov rbx, rax ;save the return of n - 1 into a temp variable
+  pop rdi ; get n back into rdi 
+  sub rdi, 2 ;set rdi up to be n - 2
+  push rbx
+  call fib
+  ;n - 2 will return here
+  mov rbp, rax
+  pop rbx
+  mov rax, rbx
+  add rax, rbp
+  ret 
--- module_1/assembly-2-b-prework/tests.c
+++ module_1/assembly-2-b-prework/tests.c
@@ -6,11 +6,9 @@ extern int index(int *matrix, int rows, int cols, int rindex, int cindex);
 extern void transpose(int *in, int *out, int rows, int cols);
 extern float volume(float radius, float height);
 
-void setUp(void) {
-}
+void setUp(void) {}
 
-void tearDown(void) {
-}
+void tearDown(void) {}
 
 void test_fib_0(void) { TEST_ASSERT_EQUAL(0, fib(0)); }
 void test_fib_1(void) { TEST_ASSERT_EQUAL(1, fib(1)); }
@@ -48,23 +46,23 @@ void test_cone_volume_1234_5678(void) {
 }
 
 int main(void) {
-    UNITY_BEGIN();
+  UNITY_BEGIN();
 
-    RUN_TEST(test_fib_0);
-    RUN_TEST(test_fib_1);
-    RUN_TEST(test_fib_2);
-    RUN_TEST(test_fib_3);
-    RUN_TEST(test_fib_10);
-    RUN_TEST(test_fib_12);
+  RUN_TEST(test_fib_0);
+  RUN_TEST(test_fib_1);
+  RUN_TEST(test_fib_2);
+  RUN_TEST(test_fib_3);
+  RUN_TEST(test_fib_10);
+  RUN_TEST(test_fib_12);
 
-    RUN_TEST(test_index_row);
-    RUN_TEST(test_index_col);
-    RUN_TEST(test_index_rect);
+  // RUN_TEST(test_index_row);
+  // RUN_TEST(test_index_col);
+  // RUN_TEST(test_index_rect);
 
-    RUN_TEST(test_cone_volume_0_0);
-    RUN_TEST(test_cone_volume_1_2);
-    RUN_TEST(test_cone_volume_55_55);
-    RUN_TEST(test_cone_volume_1234_5678);
+  // RUN_TEST(test_cone_volume_0_0);
+  // RUN_TEST(test_cone_volume_1_2);
+  // RUN_TEST(test_cone_volume_55_55);
+  // RUN_TEST(test_cone_volume_1234_5678);
 
-    return UNITY_END();
+  return UNITY_END();
 }
