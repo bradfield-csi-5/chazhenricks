820941b5eedd5f5ca795f583edb191c2c6d93fc9
Chaz Henricks <chaz.henricks@getbuilt.com>
2023-09-05 19:11:13 -0500

    lol at git

--- module_1/memory_hierarchy/metrics/metrics_test.go
+++ module_1/memory_hierarchy/metrics/metrics_test.go
@@ -7,6 +7,7 @@ import (
 
 func BenchmarkMetrics(b *testing.B) {
 	users := LoadData()
+  users2 := LoadData2()
 
 	b.Run("Average age", func(b *testing.B) {
 		actual := 0.0
@@ -18,15 +19,36 @@ func BenchmarkMetrics(b *testing.B) {
 			b.Fatalf("Expected average age to be around %.2f, not %.3f", expected, actual)
 		}
 	})
+	b.Run("Average age2", func(b *testing.B) {
+		actual := 0.0
+		for n := 0; n < b.N; n++ {
+			actual = AverageAge2(users2)
+		}
+		expected := 59.62
+		if math.IsNaN(actual) || math.Abs(actual-expected) > 0.01 {
+			b.Fatalf("Expected average age to be around %.2f, not %.3f", expected, actual)
+		}
+	})
 
 	b.Run("Average payment", func(b *testing.B) {
 		actual := 0.0
 		for n := 0; n < b.N; n++ {
-			actual = AveragePaymentAmount(users)
+	   	actual = AveragePaymentAmount(users)
 		}
 
-		expected := 499850.559
-		if math.IsNaN(actual) || math.Abs(actual-expected) > 0.01 {
+		expected := 499850.56
+		if math.IsNaN(actual) || math.Abs(actual-expected) > 0.09 {
+			b.Fatalf("Expected average payment amount to be around %.2f, not %.3f", expected, actual)
+		}
+	})
+	b.Run("Average payment2", func(b *testing.B) {
+		actual := 0.0
+		for n := 0; n < b.N; n++ {
+			actual = AveragePaymentAmount2(users2)
+		}
+
+		expected := 499850.064
+		if math.IsNaN(actual) || math.Abs(actual-expected) > 0.09 {
 			b.Fatalf("Expected average payment amount to be around %.2f, not %.3f", expected, actual)
 		}
 	})
@@ -35,6 +57,16 @@ func BenchmarkMetrics(b *testing.B) {
 		actual := 0.0
 		for n := 0; n < b.N; n++ {
 			actual = StdDevPaymentAmount(users)
+		}
+		expected := 288684.850
+		if math.IsNaN(actual) || math.Abs(actual-expected) > 0.01 {
+			b.Fatalf("Expected standard deviation to be around %.2f, not %.3f", expected, actual)
+		}
+	})
+	b.Run("Payment stddev2", func(b *testing.B) {
+		actual := 0.0
+		for n := 0; n < b.N; n++ {
+			actual = StdDevPaymentAmount2(users2)
 		}
 		expected := 288684.850
 		if math.IsNaN(actual) || math.Abs(actual-expected) > 0.01 {
--- module_1/memory_hierarchy/metrics_old.go
+++ module_1/memory_hierarchy/metrics_old.go
@@ -2,7 +2,6 @@ package metrics
 
 import (
 	"encoding/csv"
-	"fmt"
 	"log"
 	"math"
 	"os"
@@ -35,20 +34,6 @@ type User struct {
 	payments []Payment
 }
 
-// new type - all we care about for these calculations is age and payment averages
-type Users struct {
-	ages     []float64
-	payments []uint64
-}
-
-func AverageAge2(users Users) float64{
-	average, count := 0.0, 0.0
-	for _, a := range users.ages {
-		count += 1
-		average += (a - average) / count
-	}
-	return average
-}
 func AverageAge(users UserMap) float64 {
 	average, count := 0.0, 0.0
 	for _, u := range users {
@@ -69,20 +54,8 @@ func AveragePaymentAmount(users UserMap) float64 {
 	}
 	return average
 }
-func AveragePaymentAmount2(users Users) float64 {
-	average, count := 0.0, 0.0
-		for _, p := range users.payments {
-			count += 1
-			amount := float64(p / 100)
-			average += (amount - average) / count
-		}
-	return average
-}
 
 // Compute the standard deviation of payment amounts
-// per initial benchmarks, this takes the most time, so focus attention here to start.
-// variance == how spread out data is: [5,5,5] variance is 0. [5,10,15,20] variance is higher
-// variance = average(x^2 for x in data) - average(x for x in data)^2
 func StdDevPaymentAmount(users UserMap) float64 {
 	mean := AveragePaymentAmount(users)
 	squaredDiffs, count := 0.0, 0.0
@@ -96,56 +69,6 @@ func StdDevPaymentAmount(users UserMap) float64 {
 	}
 	return math.Sqrt(squaredDiffs / count)
 }
-func StdDevPaymentAmount2(users Users) float64 {
-	mean := AveragePaymentAmount2(users)
-	squaredDiffs, count := 0.0, 0.0
-		for _, p := range users.payments {
-			count += 1
-			amount := float64(p / 100)
-			diff := amount - mean
-			squaredDiffs += diff * diff
-		}
-	return math.Sqrt(squaredDiffs / count)
-}
-
-func LoadData2() Users {
-	f, err := os.Open("users.csv")
-	if err != nil {
-		log.Fatalln("Unable to read users.csv", err)
-	}
-	reader := csv.NewReader(f)
-	userLines, err := reader.ReadAll()
-	if err != nil {
-		log.Fatalln("Unable to parse users.csv as csv", err)
-	}
-
-	ages_read := make([]float64, 0)
-	for _, line := range userLines {
-		age, _ := strconv.Atoi(line[2])
-		ages_read = append(ages_read, float64(age))
-	}
-
-
-	f, err = os.Open("payments.csv")
-	if err != nil {
-		log.Fatalln("Unable to read payments.csv", err)
-	}
-	reader = csv.NewReader(f)
-	paymentLines, err := reader.ReadAll()
-	if err != nil {
-		log.Fatalln("Unable to parse payments.csv as csv", err)
-	}
-
-	payments := make([]uint64,0)
-	for _, line := range paymentLines {
-		paymentCents, _ := strconv.Atoi(line[0])
-		payments = append(payments, uint64(paymentCents))
-	}
-
-	users := Users{ages: ages_read, payments: payments}
-	return users
-
-}
 
 func LoadData() UserMap {
 	f, err := os.Open("users.csv")
@@ -158,7 +81,6 @@ func LoadData() UserMap {
 		log.Fatalln("Unable to parse users.csv as csv", err)
 	}
 
-  fmt.Printf("USERLINES: %d\n", len(userLines))
 	users := make(UserMap, len(userLines))
 	for _, line := range userLines {
 		id, _ := strconv.Atoi(line[0])
@@ -189,6 +111,5 @@ func LoadData() UserMap {
 		})
 	}
 
-  fmt.Printf("I AM ORIG USERS. AGE COUNT IS %d\n", len(users))
 	return users
 }
