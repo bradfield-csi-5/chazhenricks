20e26cbd3210c3969823b07fba97869b21bd1ab4
Chaz Henricks <chaz.henricks@getbuilt.com>
2023-11-08 18:57:47 -0600

    who even knows

--- systems_compilers/language/parser/parser.go
+++ systems_compilers/language/parser/parser.go
@@ -32,25 +32,95 @@ func (p *Parser) equality() expr.Expr {
 	for p.match(token.BANG_EQUAL, token.EQUAL_EQUAL) {
 		operator := p.previous()
 		right := p.comparison()
-		expression = expr.Binary{
+		expression = &expr.Binary{
 			Left:     expression,
 			Operator: operator,
 			Right:    right,
 		}
 	}
 
-	return expr
+	return expression
 }
 
 func (p *Parser) comparison() expr.Expr {
-	expr := p.term()
+	expression := p.term()
 
 	for p.match(token.GREATER, token.GREATER_EQUAL, token.LESS, token.LESS_EQUAL) {
 		operator := p.previous()
 		right := p.term()
-		expr = expr.Binary{Left: expr, Operator: operator, Right: right}
+		expression = &expr.Binary{Left: expression, Operator: operator, Right: right}
 	}
-	return expr
+	return expression
+}
+
+func (p *Parser) term() expr.Expr {
+	expression := p.factor()
+
+	for p.match(token.MINUS, token.PLUS) {
+		operator := p.previous()
+		right := p.unary()
+		expression = &expr.Binary{Left: expression, Operator: operator, Right: right}
+	}
+	return expression
+}
+
+func (p *Parser) factor() expr.Expr {
+	expression := p.unary()
+	for p.match(token.SLASH, token.STAR) {
+		operator := p.previous()
+		right := p.unary()
+		expression = &expr.Binary{Left: expression, Operator: operator, Right: right}
+	}
+	return expression
+}
+
+func (p *Parser) unary() expr.Expr {
+	if p.match(token.BANG, token.MINUS) {
+		operator := p.previous()
+		right := p.unary()
+		return &expr.Unary{Operator: operator, Right: right}
+	}
+
+	return p.primary()
+}
+
+func (p *Parser) primary() expr.Expr {
+	if p.match(token.FALSE) {
+		return &expr.Literal{Value: false}
+	}
+	if p.match(token.TRUE) {
+		return &expr.Literal{Value: true}
+	}
+	if p.match(token.NIL) {
+		return &expr.Literal{Value: nil}
+	}
+	if p.match(token.NUMBER, token.STRING) {
+		return &expr.Literal{Value: p.previous().Literal}
+	}
+	if p.match(token.LEFT_PAREN) {
+		expression := p.expression()
+		p.consume(token.RIGHT_PAREN, "Expect ( after expression")
+		return &expr.Grouping{Expression: expression}
+	}
+
+	//if none of these match, throw
+  return 
+}
+// Error Handling
+
+type ParseError struct {
+	Token   token.TokenEnum
+	Message string
+}
+
+func newParseError(token token.TokenEnum, message string) error {
+	return &ParseError{Token: token, Message: message}
+}
+
+
+
+func (e *ParseError) Error() string {
+	return e.Message
 }
 
 // HELPER FUNCTIONS
@@ -72,10 +142,11 @@ func (p *Parser) check(tok token.TokenEnum) bool {
 	return p.peek().Type == tok
 }
 
-func (p *Parser) advance() {
+func (p *Parser) advance() token.Token {
 	if !p.isAtEnd() {
 		p.Current += 1
 	}
+	return p.previous()
 }
 
 func (p *Parser) isAtEnd() bool {
@@ -88,4 +159,19 @@ func (p *Parser) peek() token.Token {
 
 func (p *Parser) previous() token.Token {
 	return p.Tokens[p.Current-1]
+}
+
+func (p *Parser) consume(tok token.TokenEnum, message string) token.Token {
+
+	if !p.check(tok) {
+		//throw error
+    err :=p.handleError(tok, message)
+    panic(err)
+	}
+
+	return p.advance()
+}
+
+func (p *Parser) handleError(token token.TokenEnum, message string)error {
+  return newParseError(token, message)
 }
